716760e7363e79a57735364bd38d2c80f4b5b7a4
Frank Duckart <duckart@Franks-MacBook-Pro.local>
2015-02-02 16:35:48 -1000

    Initial commit.

--- ldaprunner.hs
+++ ldaprunner.hs
@@ -4,12 +4,12 @@ main :: IO ()
 main = do 
     let ldapUri      = "ldaps://ldap.hawaii.edu:636"
         bindDn       = "cn=filedrop,ou=Specials,dc=hawaii,dc=edu"
-        baseDn       = "dc=hawaii,dc=edu"
         bindPassword = "" -- <-- Your password goes here.
-        filter       = "uid=duckart"
+        baseDn       = "dc=hawaii,dc=edu"
+        sfilter      = "uid=duckart"
     
     ldap <- ldapInitialize ldapUri
     ldapSimpleBind ldap bindDn bindPassword
     e <- ldapSearch ldap (Just baseDn) LdapScopeSubtree 
-                         (Just filter) LDAPAllUserAttrs False
+                         (Just sfilter) LDAPAllUserAttrs False
     print e
